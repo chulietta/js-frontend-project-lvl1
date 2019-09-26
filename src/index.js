40f380ec677003d71aaedceae23dd6081982df18
Yuliya <9757975@gmail.com>
2019-09-26 15:27:01 +0300

    3rd step

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import readlineSync from 'readline-sync';
 
-
-//export default readlineSync.question('May I have your name? ')
-
 export const name = () => readlineSync.question('May I have your name? ');
 
 
